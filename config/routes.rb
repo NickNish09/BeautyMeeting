6c2b0ef7cd7fa266be8202157bf6c9dd756b8d7b
nicknish09 <nnmarques97@gmail.com>
2019-02-05 23:03:17 -0200

    adicionado footer header e index de empresas

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :companies
   devise_for :companies,  controllers: {
       sessions: 'companies/sessions',
       registrations: 'companies/registrations'
